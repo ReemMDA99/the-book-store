9f09d81ebfaf805f6fc1b8a21f78ec990f0427fe
Reem Ahmed <reem.mda0909@gmail.com>
2022-07-10 00:19:41 -0600

    SignupForm.js: Replace the addUser() functionality imported from the API file with the ADD_USER mutation functionality.

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from "@apollo/client";
 
-import { createUser } from '../utils/API';
+//import { createUser } from '../utils/API';
+import { ADD_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  // Replace the addUser() functionality imported from the API file with the ADD_USER mutation functionality.
+  const [createUser] = useMutation(ADD_USER);
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
@@ -28,13 +32,15 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await addUser({
+				variables: { ...userFormData },
+			});
 
-      if (!response.ok) {
+      if (!data) {
         throw new Error('something went wrong!');
       }
 
-      const { token, user } = await response.json();
+      const { token, user } = data.addUser
       console.log(user);
       Auth.login(token);
     } catch (err) {
