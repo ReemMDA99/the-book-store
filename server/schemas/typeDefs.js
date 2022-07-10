889c383a15e9b15932f3909578b83425cd46b02c
Reem Ahmed <reem.mda0909@gmail.com>
2022-07-09 21:15:01 -0600

    add mutations and Query from the functionality in the user-controller.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,18 +6,36 @@ const { gql } = require("apollo-server-express");
         type Query {
 		me: User
 		user: User
-        
+
         }
+        type User {
+            _id: ID!
+            username: String
+            email: String
+            bookCount: Int
+            savedBooks: [Book]
+        }
+        type Book {
+            _id: ID!
+            bookId: String
+            authors: [String]
+            # authors: String
+            description: String
+            title: String
+            image: String
+            link: String
+        }
+     
 
         type Auth {
             token: ID!
             user: User
         }
+
         type Mutation {
             login(email: String!, password: String!): Auth
             addUser(username: String!, email: String!, password: String!): Auth
-            // save book
-            // remove book
+            
         }
     }
 `;
