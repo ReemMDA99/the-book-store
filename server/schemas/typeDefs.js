2470d816b756d2b2600d481cb22418c82c9cfd43
Reem Ahmed <reem.mda0909@gmail.com>
2022-07-09 22:20:33 -0600

    fix backend server issues

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,53 +2,55 @@
 
 const { gql } = require("apollo-server-express");
 
-    const typeDefs = gql`
+const typeDefs = gql`
 
-        type Query {
+    type Query {
 		me: User
 		user: User
-        }
-
-        type Mutation {
-            login(email: String!, password: String!): Auth
-            addUser(username: String!, email: String!, password: String!): Auth
-            saveBook(input: savedBook!): User
-            removeBook(bookId: ID!): User
-        }
-       
-        input savedBook {
-            _id: ID
-            bookId: String
-            title: String
-            image: String
-            link: String
-            authors: [String]
-            description: String
-            
-        }
-        type User {
-            _id: ID!
-            username: String
-            email: String
-            bookCount: Int
-            savedBooks: [Book]
-        }
-        type Book {
-            _id: ID!
-            bookId: String
-            authors: [String]
-            # authors: String
-            description: String
-            title: String
-            image: String
-            link: String
-        }
-
-        type Auth {
-            token: ID!
-            user: User
-        }
     }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: savedBook!): User
+        removeBook(bookId: ID!): User
+    }
+       
+    input savedBook {
+        _id: ID
+        bookId: String
+        title: String
+        image: String
+        link: String
+        authors: [String]
+        description: String
+            
+    }
+    
+    type User {
+        _id: ID!
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+        
+    type Book {
+        _id: ID!
+        bookId: String
+        authors: [String]
+        # authors: String
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+}
 `;
 
 module.exports = typeDefs;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,6 +6,12 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
   // function for our authenticated routes
   authMiddleware: function ({ req }) {
     // allows token to be sent via req.body, req.query, or headers
@@ -31,10 +37,6 @@ module.exports = {
 
     // send to next endpoint
     return req;
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  }
+  
 };
