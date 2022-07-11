bf4e76972c684be65b1ecba607c0f6638bf22d4f
Reem Ahmed <reem.mda0909@gmail.com>
2022-07-11 10:24:45 -0600

    delete and recreate db as the name was too long

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks-app', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/book-store', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
